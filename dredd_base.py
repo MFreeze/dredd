03960dfaa0f0b7ae3ebaae39b632a2678bf4ea29
DUVILLIE Guillerme <guillerme.duvillie@gmail.com>
2012-07-04 13:03:02 +0200

    Meilleure gestion des blagues

--- dredd_base.py
+++ dredd_base.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-import sys
 import ircbot
 import irclib
-import os
-import urllib
-import re
-from  random import randrange,randint
-from datetime import date
 
 CHAN="#stux"
 NICK="Dredd"
@@ -79,8 +73,12 @@ class DreddBase(ircbot.SingleServerIRCBot):
 
     # En cas de message privé
     def on_privmsg(self, c, e):
-        print e
-        self.do_command(e, e.arguments()[0])
+        a = e.arguments()[0].lower()
+        cmd = a.strip()
+        if cmd[0] == "!id":
+            auteur = irclib.nm_to_n(e.source())
+            complement = " ".join(cmd[1:])
+            self.id(complement, c, auteur)
 
     # En cas de connexion
     def on_join(self, c, e):
@@ -106,246 +104,12 @@ class DreddBase(ircbot.SingleServerIRCBot):
         if (qui == self.master):
             self.master = vers
 
-    def tasoeur(self, complement):
-        res = re.search(r'(le|du|la|un|une|mon|ma) ([a-zA-Zéàùôê]+)',complement)
-        if res != None:
-            if res.group(2) != None:
-                if res.group(1) in ('le', 'du', 'un', 'mon'):
-                    return "Un " + res.group(2) + " amphibien qui plus est."
-                else:
-                    return "C'est ta soeur la " + res.group(2)
-        else:
-            return None
-
-
-    def roll(self, complement, c, auteur):
-        if len(complement) != 0:
-            c.privmsg(self.channel, "%s" % (self._des(complement)))
-        else:
-            c.privmsg(self.channel, "%s" % (self._des("1d6")))
-
-    def urb(self, complement, c, auteur):
-        url = "http://urbandictionary.com/define.php?"
-        url += urllib.urlencode({"term":complement.lower()})
-        c.privmsg(self.channel, "%s" % (url))
-
-    def halp(self, complement, c, auteur):
-        c.privmsg(self.channel, "%s %s" % ("Commandes disponibles sur le chan :", " ".join(self.ls_cmd_pb)))
-        c.privmsg(self.channel, "%s %s" % ("Commandes disponibles en privé :", " ".join(self.ls_cmd_pv)))
-        if (auteur == self.master):
-            c.privmsg(self.master, "%s %s" % ("Commandes du grand maître :", " ".join(self.ls_cmd_gm)))
-
-    # Permet de savoir si Steve Jobs est encore en vie
-    def jobs(self, complement, c, auteur):
-        html = urllib.urlopen("http://en.wikipedia.org/w/api.php?action=query&titles=Steve%20Jobs&prop=categories").read()
-        rget = re.findall("deaths", html)
-        if (len(rget) > 0):
-            res = "Il est mort \o/"
-        else:
-            res = "Nope, encore en vie =("
-        c.privmsg(self.channel, res)
-
-    def bieber(self, complement, c, auteur):
-        html = urllib.urlopen('http://en.wikipedia.org/w/api.php?action=query&titles=Justin%20Bieber&prop=categories').read()
-        url = re.findall('deaths', html)
-        if len(url) != 0:
-            res = "Il est mort \o/"
-        else:
-            res = "Nope, encore en vie :("
-        c.privmsg(self.channel, res)
-
-    def get_port(self, serv):
-        if (serv in self.services):
-            return self.services[serv]
-        else:
-            return "Service inconnu"
-
-    # Teste patience, si celle ci atteint 0, on kicke
-    def _patience(self, c, auteur):
-        self.patience -= 1
-        if self.patience <= 0:
-            c.kick(self.channel,auteur, "Le chan est désormais pacifié.")
-            self.patience = INIT_PATIENCE
-
-    # Teste si un lien est down
-    def is_down(self, complement, c, auteur):
-        mes = ""
-        try:
-            url_obj = urllib.urlopen(complement)
-            if url_obj.getcode() == 200:
-                mes += "Ta connexion est en carton."
-            elif url_obj.getcode() == 404:
-                mes += "Ca n'existe pas sur le serveur."
-            else:
-                mes += "Ca ne marche pas."
-        except:
-            mes += "Une url valide ça te dit quelque chose?"
-            self._patience(c, auteur)
-            return mes
-
-    def pop(self, complement, c, auteur):
-        if len(self.pile) > 0:
-            c.privmsg(self.channel, "%s - %s left" % (self.pile.pop(), str(len(self.pile))))
-        else:
-            c.privmsg(self.channel, "Rien dans la pile")
-            self._patience(c, auteur)
-
-    def wp(self, complement, c, auteur):
-        url = "http://en.wikipedia.org/w/index.php?"
-        url += urllib.urlencode({"search":complement.lower()})
-        c.privmsg(self.channel, "%s" % (url))
-
-    def wpf(self, complement, c, auteur):
-        url = "http://fr.wikipedia.org/w/index.php?"
-        url += urllib.urlencode({"search":complement.lower()})
-        c.privmsg(self.channel, "%s" % (url))
-
-    def enfr(self, complement, c, auteur):
-        url = "http://www.wordreference.com/enfr/" 
-        url += urllib.pathname2url(complement.lower())
-        c.privmsg(self.channel, "%s" % (url))
-
-    def fren(self, complement, c, auteur):
-        url = "http://www.wordreference.com/fren/" 
-        url += urllib.pathname2url(complement.lower())
-        c.privmsg(self.channel, "%s" % (url))
-
-    def popall(self, complement, c, auteur):
-        while len(self.pile) > 0:
-            self.pop(complement, c, auteur)
-        self.pop(complement, c, auteur)
-
-    def port(self, complement, c, auteur):
-        c.privmsg(self.channel, "%s : %s" % complement, self.get_port(complement.lower()))
-
-    def down(self, complement, c, auteur):
-        c.privmsg(self.channel, self.is_down(complement, auteur, c))
-
-    def weekend(self, complement, c, auteur):
-        c.privmsg(self.channel, WEEKEND[date.today().weekday()])
-
-    # Gestion des commandes publiques
-    def execute_action(self, e, arg):
-        c = self.connection
-        auteur = irclib.nm_to_n(e.source())
-
-        cmd = arg.split()
-
-        if cmd[0] in self.ls_cmd_pb:
-            # Récupération des arguments de la commande
-            complement = " ".join(cmd[1:])
-            getattr(self, cmd[0][1:])(complement, c, auteur)
-
-
-
-    # Gestion des blagues
-    def execute_sentence(self, e, blague):
-        c = self.connection
-        auteur = irclib.nm_to_n(e.source())
-
-        if (blague == "a+" or blague == "++"):
-            c.privmsg(self.channel, "Je savais que vous alliez dire ça.")
-        elif (blague == "marie france" or blague == "marie-france"):
-            c.privmsg(self.channel, "Mmmmmmmh, Marie France :)")
-        elif (blague == "sasha"):
-            c.privmsg(self.channel, "<3")
-        elif (blague == "pute"):
-            c.privmsg(self.channel, "Juge pute!")
-        elif (blague == "concentré" or blague == "concentrer"):
-            c.privmsg(self.channel, "T'as qu'à aller dans un camps !")
-        elif (blague == "xd"):
-            c.kick(self.channel, auteur, "MEIN LEBENSRAUM \o/")
-        elif (blague == "chmod"):
-            c.kick(self.channel, auteur, "La séance est levée.")
-        elif (blague == "kikoo"):
-            c.kick(self.channel, auteur, "Désolé, j'ai mes règles.")
-        elif (blague == "arwen"):
-            c.kick(self.channel, auteur, "Leia > *")
-        elif (blague == "soeur"):
-            c.privmsg(self.channel, "Ta soeur porte des leggings")
-        elif (blague == "chmod 777"):
-            c.kick(self.channel, auteur, "PUTAIIIIIIN")
-
-    def saychan(self, complement, c, auteur):
-        c.privmsg(self.channel, complement)
-
-    # TODO: Si complement == "", kicke un type au hasard
-    def kick(self, complement, c, auteur):
-        c.kick(self.channel, complement, "Dans ta face ...")
-        self.patience += 2
-    
-    def op(self, complement, c, auteur):
-        if complement == "":
-            complement = auteur
-        c.mode(self.channel, "+o %s" % complement)
-
-    def update(self, complement, c, auteur):
-        os.system("sleep 2 && %s a" % sys.argv[0])
-        c.disconnect(self.channel, "I'll be back!")
-        sys.exit(0)
-
-    def quit(self, complement, c, auteur):
-        c.disconnect(self.channel, "I WILL SURVIVE!")
-        sys.exit(0)
-
-    def topic(self, complement, c, auteur):
-        c.topic(self.channel, complement)
-
-    def push(self, complement, c, auteur):
-        if len(self.pile) < PILE_MAX_SIZE:
-            self.pile.append(complement)
-            self.patience += 1
-            c.privmsg(auteur, "C'est noté.")
-        else:
-            c.privmsg(auteur, "Pile pleine!")
-            self._patience(c, auteur)
-
     def id(self, complement, c, auteur):
         if complement == self.masterpassword:
             self.master = auteur
             c.privmsg(auteur, "=)")
         else:
             c.privmsg(auteur, "Fous toi ton %s où je pense!" % complement)
-            self._patience(c, auteur)
-
-    # Liste des actions réalisées en réponse à des messages privés
-    def do_command(self, e, arg):
-
-        # Utilisation du masque irclib.nm_to_n pour récupérer l'auteur du message
-        auteur = irclib.nm_to_n(e.source())
-        c = self.connection
-
-        cmd = arg.split()
-        complement = ""
-        boo = False
-
-
-        # On vérifie la validité de la commande
-        if cmd[0] in self.ls_cmd_pv or cmd[0] in self.ls_cmd_gm:
-
-            # Récupération des arguments de la commande
-            complement = " ".join(cmd[1:])
-            if cmd[0] in self.ls_cmd_gm :
-                if auteur != self.master:
-                    c.kick(self.channel, auteur, "Fallait pas me prendre pour un con.")
-                    self.patience += 2
-                    boo = True
-                else:
-                    try:
-                        boo = True
-                        getattr(self, cmd[0][1:])(complement, c, auteur) 
-                    except:
-                        pass
-            else:
-                try:
-                    boo = True
-                    getattr(self, cmd[0][1:])(complement, c, auteur) 
-                except:
-                    pass
-        if not boo:
-            c.privmsg(auteur, "Mais ta gueule !")
-            self._patience(c, auteur)
-
 
 if __name__ == "__main__":
     dredd = DreddBase(CHAN, NICK, SERV, PORT, HELLO, MASTER, MASTERPASS, OPPASSWD, OPLOGIN)
--- dredd_func.py
+++ dredd_func.py
@@ -2,7 +2,6 @@
 #-*- coding: utf-8 -*-
 
 import sys
-import ircbot
 import irclib
 import os
 import urllib
@@ -11,12 +10,35 @@ import dredd_base as dr
 from  random import randrange,randint
 from datetime import date
 
+CHAN="#stux"
+NICK="Dredd"
+SERV="dadaist.com"
+PORT=1664
+HELLO="Ici la loi c'est moi."
+OPPASSWD="BITE"
+OPLOGIN="BITE"
+
 LISTE_CMD_PUB = ["!pop", "!roll", "!enfr", "!fren", "!wp", "!wpf", "!urb", "!port",
                  "!halp", "!down", "!jobs", "!bieber", "!weekend", "!dredd", "!popall"]
 LISTE_CMD_PRIV = ["!push", "!id"]
 LISTE_CMD_MASTER = ["!update", "!kick", "!topic", "!saychan", "!op"]
-LISTE_BLAGUE = ["a+", "marie france", "marie-france", "sasha", "soeur", "pute", "concentré",
-                "concentrer", "xd", "chmod", "><", "kikoo", "arwen", "chmod 777"]
+LISTE_BLAGUE = {"a+":("privmsg", "Je savais que vous alliez dire ça."),
+                "++":("privmsg", "Je savais que vous alliez dire ça."),
+                "marie france":("privmsg", "Mmmmmmmh, Marie France :)"),
+                "marie-france":("privmsg", "Mmmmmmmh, Marie France :)"), 
+                "sasha":("privmsg", "<3"), 
+                "soeur":("privmsg", "Ta soeur porte des leggings!"), 
+                "pute":("privmsg", "Juge pute!"),
+                "concentré":("privmsg", "T'as qu'à aller dans un camps!"),
+                "concentrer":("privmsg", "T'as qu'à aller dans un camps!"), 
+                "xd":("kick", "MEIN LEBENSRAUM o/"), 
+                "chmod":("kick", "La séance est levée."),
+                "><":("kick", "Tas de cons!"), 
+                "kikoo":("kick", "Désolé j'ai mes règles"), 
+                "arwen":("kick", "Leia > *"),
+                "chmod 777":("kick", "PUTAIIIIIIIIN"),
+                "^^":("kick", "I'm back!")}
+
 PILE_MAX_SIZE = 16
 INIT_PATIENCE = 4
 WEEKEND = ['Déjà ?', 'T\'en est loin coco.', 'Nope :(', 'ça vient !', 'Preque \o/', 'Mais on est déjà en weekend, va te biturer !', 'C\'est déjà presque finis :(']
@@ -49,12 +71,17 @@ class Dredd(dr.DreddBase):
         num = int(dice[0])
         return sum(randrange(sides)+1 for die in range(num))
 
+    # En cas de message privé
+    def on_privmsg(self, c, e):
+        print e
+        self.do_command(e, e.arguments()[0])
+
     # En cas de messages publiés sur le channel
     def on_pubmsg(self, c, e):
         a = e.arguments()[0].lower()
 
         # On cherche dans la phrase s'il y a une blague à faire
-        for i in (self.ls_blague):
+        for i in (self.ls_blague.keys()):
             if ((not a.find(i) == -1) and not ( irclib.nm_to_n(e.source()) == self.getnickname())) :
                 self.execute_sentence(e, i)
                 break
@@ -72,3 +99,225 @@ class Dredd(dr.DreddBase):
     def dredd(self, complement, c, auteur):
         c.privmsg(self.channel, "La version 1 est morte...")
 
+    def tasoeur(self, complement):
+        res = re.search(r'(le|du|la|un|une|mon|ma) ([a-zA-Zéàùôê]+)',complement)
+        if res != None:
+            if res.group(2) != None:
+                if res.group(1) in ('le', 'du', 'un', 'mon'):
+                    return "Un " + res.group(2) + " amphibien qui plus est."
+                else:
+                    return "C'est ta soeur la " + res.group(2)
+        else:
+            return None
+
+    def roll(self, complement, c, auteur):
+        if len(complement) != 0:
+            c.privmsg(self.channel, "%s" % (self._des(complement)))
+        else:
+            c.privmsg(self.channel, "%s" % (self._des("1d6")))
+
+    def urb(self, complement, c, auteur):
+        url = "http://urbandictionary.com/define.php?"
+        url += urllib.urlencode({"term":complement.lower()})
+        c.privmsg(self.channel, "%s" % (url))
+
+    def halp(self, complement, c, auteur):
+        c.privmsg(self.channel, "%s %s" % ("Commandes disponibles sur le chan :", " ".join(self.ls_cmd_pb)))
+        c.privmsg(self.channel, "%s %s" % ("Commandes disponibles en privé :", " ".join(self.ls_cmd_pv)))
+        if (auteur == self.master):
+            c.privmsg(self.master, "%s %s" % ("Commandes du grand maître :", " ".join(self.ls_cmd_gm)))
+
+    # Permet de savoir si Steve Jobs est encore en vie
+    def jobs(self, complement, c, auteur):
+        html = urllib.urlopen("http://en.wikipedia.org/w/api.php?action=query&titles=Steve%20Jobs&prop=categories").read()
+        rget = re.findall("deaths", html)
+        if (len(rget) > 0):
+            res = "Il est mort \o/"
+        else:
+            res = "Nope, encore en vie =("
+        c.privmsg(self.channel, res)
+
+    def bieber(self, complement, c, auteur):
+        html = urllib.urlopen('http://en.wikipedia.org/w/api.php?action=query&titles=Justin%20Bieber&prop=categories').read()
+        url = re.findall('deaths', html)
+        if len(url) != 0:
+            res = "Il est mort \o/"
+        else:
+            res = "Nope, encore en vie :("
+        c.privmsg(self.channel, res)
+
+    def get_port(self, serv):
+        if (serv in self.services):
+            return self.services[serv]
+        else:
+            return "Service inconnu"
+
+    # Teste patience, si celle ci atteint 0, on kicke
+    def _patience(self, c, auteur):
+        self.patience -= 1
+        if self.patience <= 0:
+            c.kick(self.channel,auteur, "Le chan est désormais pacifié.")
+            self.patience = INIT_PATIENCE
+
+    # Teste si un lien est down
+    def is_down(self, complement, c, auteur):
+        mes = ""
+        try:
+            url_obj = urllib.urlopen(complement)
+            if url_obj.getcode() == 200:
+                mes += "Ta connexion est en carton."
+            elif url_obj.getcode() == 404:
+                mes += "Ca n'existe pas sur le serveur."
+            else:
+                mes += "Ca ne marche pas."
+        except:
+            mes += "Une url valide ça te dit quelque chose?"
+            self._patience(c, auteur)
+            return mes
+
+    def pop(self, complement, c, auteur):
+        if len(self.pile) > 0:
+            c.privmsg(self.channel, "%s - %s left" % (self.pile.pop(), str(len(self.pile))))
+        else:
+            c.privmsg(self.channel, "Rien dans la pile")
+            self._patience(c, auteur)
+
+    def wp(self, complement, c, auteur):
+        url = "http://en.wikipedia.org/w/index.php?"
+        url += urllib.urlencode({"search":complement.lower()})
+        c.privmsg(self.channel, "%s" % (url))
+
+    def wpf(self, complement, c, auteur):
+        url = "http://fr.wikipedia.org/w/index.php?"
+        url += urllib.urlencode({"search":complement.lower()})
+        c.privmsg(self.channel, "%s" % (url))
+
+    def enfr(self, complement, c, auteur):
+        url = "http://www.wordreference.com/enfr/" 
+        url += urllib.pathname2url(complement.lower())
+        c.privmsg(self.channel, "%s" % (url))
+
+    def fren(self, complement, c, auteur):
+        url = "http://www.wordreference.com/fren/" 
+        url += urllib.pathname2url(complement.lower())
+        c.privmsg(self.channel, "%s" % (url))
+
+    def popall(self, complement, c, auteur):
+        while len(self.pile) > 0:
+            self.pop(complement, c, auteur)
+        self.pop(complement, c, auteur)
+
+    def port(self, complement, c, auteur):
+        c.privmsg(self.channel, "%s : %s" % complement, self.get_port(complement.lower()))
+
+    def down(self, complement, c, auteur):
+        c.privmsg(self.channel, self.is_down(complement, auteur, c))
+
+    def weekend(self, complement, c, auteur):
+        c.privmsg(self.channel, WEEKEND[date.today().weekday()])
+
+    # Gestion des commandes publiques
+    def execute_action(self, e, arg):
+        c = self.connection
+        auteur = irclib.nm_to_n(e.source())
+
+        cmd = arg.split()
+
+        if cmd[0] in self.ls_cmd_pb:
+            # Récupération des arguments de la commande
+            complement = " ".join(cmd[1:])
+            getattr(self, cmd[0][1:])(complement, c, auteur)
+
+
+
+    # Gestion des blagues
+    def execute_sentence(self, e, blague):
+        c = self.connection
+        auteur = irclib.nm_to_n(e.source())
+        action = self.ls_blague[blague]
+        if action[0] != "privmsg":
+            getattr(c, action[0])(self.channel, action[1])
+        else:
+            getattr(c, action[0])(self.channel, auteur, action[1])
+
+    def saychan(self, complement, c, auteur):
+        c.privmsg(self.channel, complement)
+
+    # TODO: Si complement == "", kicke un type au hasard
+    def kick(self, complement, c, auteur):
+        c.kick(self.channel, complement, "Dans ta face ...")
+        self.patience += 2
+    
+    def op(self, complement, c, auteur):
+        if complement == "":
+            complement = auteur
+        c.mode(self.channel, "+o %s" % complement)
+
+    def update(self, complement, c, auteur):
+        os.system("sleep 2 && %s a" % sys.argv[0])
+        c.disconnect(self.channel, "I'll be back!")
+        sys.exit(0)
+
+    def quit(self, complement, c, auteur):
+        c.disconnect(self.channel, "I WILL SURVIVE!")
+        sys.exit(0)
+
+    def topic(self, complement, c, auteur):
+        c.topic(self.channel, complement)
+
+    def push(self, complement, c, auteur):
+        if len(self.pile) < PILE_MAX_SIZE:
+            self.pile.append(complement)
+            self.patience += 1
+            c.privmsg(auteur, "C'est noté.")
+        else:
+            c.privmsg(auteur, "Pile pleine!")
+            self._patience(c, auteur)
+
+    def id(self, complement, c, auteur):
+        if complement == self.masterpassword:
+            self.master = auteur
+            c.privmsg(auteur, "=)")
+        else:
+            c.privmsg(auteur, "Fous toi ton %s où je pense!" % complement)
+            self._patience(c, auteur)
+
+    # Liste des actions réalisées en réponse à des messages privés
+    def do_command(self, e, arg):
+        # Utilisation du masque irclib.nm_to_n pour récupérer l'auteur du message
+        auteur = irclib.nm_to_n(e.source())
+        c = self.connection
+
+        cmd = arg.split()
+        complement = ""
+        boo = False
+
+        # On vérifie la validité de la commande
+        if cmd[0] in self.ls_cmd_pv or cmd[0] in self.ls_cmd_gm:
+
+            # Récupération des arguments de la commande
+            complement = " ".join(cmd[1:])
+            if cmd[0] in self.ls_cmd_gm :
+                if auteur != self.master:
+                    c.kick(self.channel, auteur, "Fallait pas me prendre pour un con.")
+                    self.patience += 2
+                    boo = True
+                else:
+                    try:
+                        boo = True
+                        getattr(self, cmd[0][1:])(complement, c, auteur) 
+                    except:
+                        pass
+            else:
+                try:
+                    boo = True
+                    getattr(self, cmd[0][1:])(complement, c, auteur) 
+                except:
+                    pass
+        if not boo:
+            c.privmsg(auteur, "Mais ta gueule !")
+            self._patience(c, auteur)
+
+if __name__ == "__main__":
+    dredd = Dredd(CHAN, NICK, SERV, PORT, HELLO, MASTER, MASTERPASS, OPPASSWD, OPLOGIN)
+    dredd.start()
